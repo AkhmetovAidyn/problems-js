cd1ad9f7acae4f09e9b49f81664554481f738333
uchenik1 <ahmetovv_aidyn@mail.ru>
2023-09-04 01:10:18 +0600

    problem3

--- exercise1/problem3/index.js
+++ exercise1/problem3/index.js
@@ -1,5 +1,18 @@
-function pentagonal() {
+function pentagonal(n) {
   // Your code
+  if (n <= 0) {
+    return 0;
+  }
+  return 1 + (5 * (n - 1) * (n - 2)) / 2;
 }
 
 module.exports = pentagonal;
+
+
+console.log(pentagonal(1)) // 1
+
+console.log(pentagonal(2)) // 6
+
+console.log(pentagonal(3)) // 16
+
+console.log(pentagonal(8)) // 141
