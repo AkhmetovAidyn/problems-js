fc4e2cf554c55200fa225d1cafb62cb8eea200e0
uchenik1 <ahmetovv_aidyn@mail.ru>
2023-09-04 01:21:33 +0600

    problem5

--- exercise1/problem5/index.js
+++ exercise1/problem5/index.js
@@ -1,5 +1,15 @@
-function removeABC() {
+function removeABC(str) {
   // Your code
+  const result = str.replace(/[abc]/g, '');
+
+  if (result === str) {
+    return null;
+  }
+  return result;
 }
+
+console.log(removeABC("This might be a bit hard")); // "This might e  it hrd"
+console.log(removeABC("hello world!")); // null
+console.log(removeABC("")); // null
 
 module.exports = removeABC;
