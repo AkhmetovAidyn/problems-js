c18b1e4f82748f251a5e96370c3adcba0abb7a7b
uchenik1 <ahmetovv_aidyn@mail.ru>
2023-09-04 01:41:30 +0600

    problem6

--- exercise1/problem6/index.js
+++ exercise1/problem6/index.js
@@ -1,5 +1,13 @@
-function insertWhitespace() {
+function insertWhitespace(str) {
   // Your code
+  const result = str.replace(/([a-z])([A-Z])/g, '$1 $2');
+  return result;
 }
+
+console.log(insertWhitespace("SheWalksToTheBeach")) // "She Walks To The Beach"
+
+console.log(insertWhitespace("MarvinTalksTooMuch")) // "Marvin Talks Too Much"
+
+console.log(insertWhitespace("TheGreatestUpsetInHistory")) // "The Greatest Upset In History"
 
 module.exports = insertWhitespace;
