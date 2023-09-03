43a8e380ad52b96e0d70dd80570bbc7a68bed9b6
uchenik1 <ahmetovv_aidyn@mail.ru>
2023-09-04 02:25:51 +0600

    problem7

--- exercise1/problem7/index.js
+++ exercise1/problem7/index.js
@@ -1,5 +1,17 @@
-function isPrimitive() {
-  // Your code
+function isPrimitive(value) {
+  return value !== null && value !== "";
 }
+
+console.log(isPrimitive(function add(x, y) {
+  return x + y;
+})); // true
+
+console.log(isPrimitive(new RegExp('^[a-zA-Z0-9]+$', 'g'))); // true
+
+console.log(isPrimitive(null)); // false
+
+console.log(isPrimitive("")); // false
+
+
 
 module.exports = isPrimitive;
