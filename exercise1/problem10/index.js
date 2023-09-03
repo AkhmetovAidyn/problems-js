ec15abd935b8a5795d7095d342eb63e42af393ad
uchenik1 <ahmetovv_aidyn@mail.ru>
2023-09-04 02:43:54 +0600

    problem10

--- exercise1/problem10/index.js
+++ exercise1/problem10/index.js
@@ -1,5 +1,24 @@
-function move() {
-  // Your code
+function move(str) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let result = '';
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    const index = alphabet.indexOf(char);
+
+    if (index !== -1) {
+      const nextIndex = (index + 1) % alphabet.length;
+      result += alphabet[nextIndex];
+    } else {
+      result += char;
+    }
+  }
+
+  return result;
 }
+
+console.log(move("hello")); // "ifmmp"
+console.log(move("bye")); // "czf"
+console.log(move("welcome")); // "xfmdpnf"
 
 module.exports = move;
