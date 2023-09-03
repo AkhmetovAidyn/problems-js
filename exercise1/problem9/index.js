04c786b651462f0fc903da640bc9e71ba293951d
uchenik1 <ahmetovv_aidyn@mail.ru>
2023-09-04 02:41:49 +0600

    problem9

--- exercise1/problem9/index.js
+++ exercise1/problem9/index.js
@@ -1,5 +1,20 @@
-function isDisarium() {
-  // Your code
+function isDisarium(num) {
+  const numStr = num.toString();
+  let sum = 0;
+
+  for (let i = 0; i < numStr.length; i++) {
+    const digit = parseInt(numStr[i]);
+    sum += Math.pow(digit, i + 1);
+  }
+
+  return sum === num;
 }
+
+console.log(isDisarium(75)); // false
+console.log(isDisarium(135)); // true
+console.log(isDisarium(544)); // false
+console.log(isDisarium(518)); // true
+console.log(isDisarium(8)); // true
+console.log(isDisarium(466)); // false
 
 module.exports = isDisarium;
