146f12f15b3167667acb89b9ffbdb40382393cc1
uchenik1 <ahmetovv_aidyn@mail.ru>
2023-09-04 01:16:40 +0600

    problem4

--- exercise1/problem4/index.js
+++ exercise1/problem4/index.js
@@ -1,5 +1,18 @@
-function makeTitleCase() {
+function makeTitleCase(str) {
   // Your code
+  const words = str.split(" ");
+  const titleCaseWords = words.map(word => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  });
+  return titleCaseWords.join(" ");
 }
+
+console.log(makeTitleCase("This is a title")) // "This Is A Title"
+
+console.log(makeTitleCase("capitalize every word")) // "Capitalize Every Word"
+
+console.log(makeTitleCase("I Like Pizza")) // "I Like Pizza"
+
+console.log(makeTitleCase("PIZZA PIZZA PIZZA")) // "PIZZA PIZZA PIZZA"
 
 module.exports = makeTitleCase;
