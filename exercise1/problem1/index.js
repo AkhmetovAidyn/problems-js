4c7590e723e4dd3f5af7a1c051fbf6c83d18d0be
uchenik1 <ahmetovv_aidyn@mail.ru>
2023-09-03 23:29:47 +0600

    problem1

--- exercise1/problem1/index.js
+++ exercise1/problem1/index.js
@@ -1,5 +1,16 @@
 function numOfDigits(int) {
   // Your code
+  return int.toString().length;
 }
+
+
+
+console.log(numOfDigits(1000)) // 4
+
+console.log(numOfDigits(12)) // 2
+
+console.log(numOfDigits(1305981031)) // 10
+
+console.log(numOfDigits(0)) // 1
 
 module.exports = numOfDigits;
