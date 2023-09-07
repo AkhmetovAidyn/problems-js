9e712438fd68b67e0671ebc28bf81962eab6fa1a
uchenik1 <ahmetovv_aidyn@mail.ru>
2023-09-07 12:04:52 +0600

    problem8

--- exercise1/problem8/index.js
+++ exercise1/problem8/index.js
@@ -1,19 +1,17 @@
 function longestTime(h, m, s) {
-  const totalSeconds = h * 3600 + m * 60 + s;
-  const maxTimeInSeconds = Math.max(h * 3600, m * 60, s);
-  if (maxTimeInSeconds === h * 3600) {
+  if(h * 3600 > m * 60 && h * 3600 > s) {
     return h;
-  } else if (maxTimeInSeconds === m * 60) {
+  } else if (m * 60 > h * 3600 && m * 60 > s) {
     return m;
   } else {
-    return s;
+    return s
   }
 }
 
-console.log(longestTime(1, 59, 3598)); // 1
-console.log(longestTime(2, 300, 15000)); // 300
-console.log(longestTime(15, 955, 59400)); // 59400
 
+console.log(longestTime(1, 59, 3598));  // 1
+console.log(longestTime(2, 300, 15000));  // 300
+console.log(longestTime(15, 955, 59400));  // 59400
 
 
 module.exports = longestTime;
