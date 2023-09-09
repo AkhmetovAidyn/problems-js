7754b572ea750e39f85afe853d85c8c8abf49974
AkhmetovAidyn <ahmetovv_aidyn@mail.ru>
2023-09-09 07:08:31 +0600

    update2

--- exercise2/problem4/index.js
+++ exercise2/problem4/index.js
@@ -1,5 +1,19 @@
-function numInStr() {
+function numInStr(arr) {
   // Your code
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const str = arr[i];
+    if (/\d/.test(str)) {
+      result.push(str)
+    }
+  }
+  return result;
 }
+
+console.log(numInStr(["1a", "a", "2b", "b"])); // ["1a", "2b"]
+console.log(numInStr(["abc", "abc10"])); // ["abc10"]
+console.log(numInStr(["abc", "ab10c", "a10bc", "bcd"])); // ["ab10c", "a10bc"]
+console.log(numInStr(["this is a test", "test1"])); // ["test1"]
 
 module.exports = numInStr;
--- exercise2/problem6/index.js
+++ exercise2/problem6/index.js
@@ -1,5 +1,15 @@
-function mapping() {
+function mapping(arr) {
   // Your code
+  const result = {};
+
+  for (const letter of arr) {
+    result[letter] = letter.toUpperCase();
+  }
+  return result;
 }
+
+console.log(mapping(["p", "s"])); // { "p": "P", "s": "S" }
+console.log(mapping(["a", "b", "c"])); // { "a": "A", "b": "B", "c": "C" }
+console.log(mapping(["a", "v", "y", "z"])); // { "a": "A", "v": "V", "y": "Y", "z": "Z" }
 
 module.exports = mapping;
